182958062d294d64a2adabae48569774e509d0d2
Geoff Williams <geoff.williams@puppetlabs.com>
2015-07-30 12:03:51 +1000

    only proceed on success

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PATH="/opt/puppetlabs/puppet/bin:/opt/puppet/bin:$PATH"
 PUPPET_APPLY="puppet apply --modulepath=$(pwd)/support_modules:$(puppet config print basemodulepath) -e "
-$PUPPET_APPLY "include setup::prepare"
-$PUPPET_APPLY "include setup::install"
+$PUPPET_APPLY "include setup::prepare" && \
+$PUPPET_APPLY "include setup::install" && \
 $PUPPET_APPLY "include setup::cleanup"
