b22d97609749d65f3e4969a24beecf93fcd8d5bf
Geoff Williams <geoff.williams@puppetlabs.com>
2015-08-04 16:03:04 +1000

    variable order

--- site/profiles/manifests/puppet/r10k_mcollective_client.pp
+++ site/profiles/manifests/puppet/r10k_mcollective_client.pp
@@ -3,9 +3,16 @@ class profiles::puppet::r10k_mcollective_client(
     $user_home = hiera("profiles::puppet::r10k_mcollective_client::user_home"),
     $activemq_brokers = hiera("profiles::puppet::r10k_mcollective_client::activemq_brokers"),
     $logfile = false,
-    $cert_name = "${::fqdn}__${user_name}",
+    $cert_name = false,
 ) {
-  
+
+  if $cert_name {
+    $_cert_name = $cert_name
+  } else {
+    # can't be done in parametes, user_name has not been evaluated yet
+    $_cert_name = "${::fqdn}__${user_name}"
+  } 
+ 
   # If user supplies custom logdir use it and have user be responsible for
   # creating directory structure, etc.  Otherwise just create a directory
   # under /var/log and allow access
