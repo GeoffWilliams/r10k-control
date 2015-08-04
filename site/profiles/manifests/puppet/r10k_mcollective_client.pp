e5de034f4e0124ac3d47343b04b82d47b74566a6
Geoff Williams <geoff.williams@puppetlabs.com>
2015-08-04 12:44:37 +1000

    fix mco brokers, add note how to configure

--- site/profiles/manifests/puppet/r10k_mcollective_client.pp
+++ site/profiles/manifests/puppet/r10k_mcollective_client.pp
@@ -1,6 +1,7 @@
 class profiles::puppet::r10k_mcollective_client(
     $user_name = hiera("profiles::puppet::r10k_mcollective_client::user_name"),
     $user_home = hiera("profiles::puppet::r10k_mcollective_client::user_home"),
+    $mcollective_brokers = hiera("profiles::puppet::r10k_mcollective_client::activemq_brokers"),
 ) {
   
   # r10k mco plugin
@@ -8,8 +9,9 @@ class profiles::puppet::r10k_mcollective_client(
 
   # MCO certifcates and client
   puppet_enterprise::mcollective::client { $user_name:
-    create_user => false,
-    home_dir     => $user_home,
+    mcollective_brokers => $mcollective_brokers,
+    create_user         => false,
+    home_dir            => $user_home,
   }   
   
 }
