4cf1c423d6c3e27d8bc71f083f7baa7c0651c9a8
Geoff Williams <geoff.williams@puppetlabs.com>
2015-08-13 21:04:00 +1000

    allow select ntp servers

--- site/profiles/manifests/base.pp
+++ site/profiles/manifests/base.pp
@@ -1,8 +1,11 @@
-class profiles::base {
+class profiles::base(
+    $ntp_servers = hiera("profiles::base::ntp_servers", undef),
+) {
   include profiles::puppet::agent
   include profiles::systemd
 
   class { "ntp":
     disable_monitor => true,
+    servers         => $ntp_servers,
   }
 }
