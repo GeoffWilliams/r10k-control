3e097ab051cddbf3a01a631becf8cc2625ccdbd4
Geoff Williams <geoff.williams@puppetlabs.com>
2015-09-16 16:50:23 +1000

    more crud cleanup

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -59,13 +59,3 @@ if profile_classes then
   end
 end
 
-
-#describe "profiles::motd" do
-#  it "should bloody work" do
-#
-#    exit_code = apply_manifest('include profiles::motd', :catch_failures => true).exit_code
-#    expect(exit_code).to_not eq(1) # generic error code - cant be right...
-#    expect(exit_code).to_not eq(4) # failures
-#    expect(exit_code).to_not eq(6) # failures and changes
-#  end
-#end
