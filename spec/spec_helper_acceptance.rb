9cef07b0b4570854f6ef8f8b96d31bb1009fceaa
Geoff Williams <geoff.williams@puppetlabs.com>
2015-10-09 16:51:36 +1100

    use my own scp

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -10,7 +10,15 @@ hosts.each do |host|
   # bootstrap puppet code
   on host, "mkdir -p /etc/facter/facts.d && echo 'is_beaker=true' > /etc/facter/facts.d/beaker.txt"
 
-  scp_to host, "#{proj_root}", "/root",  {:verbose => :debug}
+#  scp_to host, "#{proj_root}", "/root",  {:verbose => :debug}
+
+  Net::SSH.start(host["ip"], host[:ssh][:user], {:password =>  host[:ssh][:password], :port => host[:ssh][:port]}) do |ssh|
+    ssh.scp.upload!(proj_root, "/root", :recursive => true) do |ch, name, sent, total|
+      puts "#{name}: #{sent}/#{total}"
+    end
+  end
+
+
 
   # bootstrap!  Note that initial 'test' hieradata is selected in
   on host, "cd /root/r10k-control && ./bootstrap.sh"
