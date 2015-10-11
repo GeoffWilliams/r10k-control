156bf0681a99f6d9081d6928fba6a50c3296b072
Geoff Williams <geoff@geoffwilliams.me.uk>
2015-10-12 00:12:09 +1100

    extra syntaxing

--- site/profiles/manifests/vim.pp
+++ site/profiles/manifests/vim.pp
@@ -1,3 +1,9 @@
+# Setup a nice version of vim
 class profiles::vim {
   class { 'vim': }
+
+  # install extra bells and whistles for debian
+  if $::osfamily == debian {
+    package { ["vim-syntax-docker", "vim-syntax-go", "vim-puppet"]: }
+  }
 }
