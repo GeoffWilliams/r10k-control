f8d6cfde62cbb1fd09757072bb40d73794a58334
Geoff Williams <geoff.williams@puppetlabs.com>
2016-10-04 22:04:12 +1100

    top scope firewall defaults

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,6 +4,16 @@ if $::kernel == 'windows' {
   Package { provider => chocolatey, }
 }
 
+
+resources { 'firewall':
+  purge => true,
+}
+
+Firewall {
+  before  => Class['r_profile::fw::post'],
+  require => Class['r_profile::fw::pre'],
+}
+
 node default {
   include r_role::trusted_fact_classified
 }
