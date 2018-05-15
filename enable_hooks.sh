742db1846f4de622c28a0652ffd6f2b2d182d712
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-05-15 23:28:47 +1000

    hiera funs

--- enable_hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Enable all supplied git hooks.  Cannot be done automatically due to security
-# concerns.  Developers working on this module are recommended to run this 
-# script once code has been checked out to prevent committing code with syntax
-# errors
-for FILE in hooks/* ; do
-  cp $FILE .git/$FILE
-done
